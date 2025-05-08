75e0996a150b28537c912b7aca82f82ca4ffcab3
Mang00o <soci.alex24@gmail.com>
2025-05-08 22:57:45 +0200

    add next workout button

--- lib/main.dart
+++ lib/main.dart
@@ -7,49 +7,75 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  Future<Map<String, dynamic>> loadWorkout() async {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WorkoutScreen(),
+    );
+  }
+}
+
+class WorkoutScreen extends StatefulWidget {
+  @override
+  _WorkoutScreenState createState() => _WorkoutScreenState();
+}
+
+class _WorkoutScreenState extends State<WorkoutScreen> {
+  List<dynamic> days = [];
+  int currentDayIndex = 0;
+  int month = 0;
+  int week = 0;
+
+  Future<void> loadWorkout() async {
     final jsonString = await rootBundle.loadString('assets/workouts.json');
     final data = json.decode(jsonString);
-    final month = data['month'];
-    final week = data['weeks'][0]['week'];
-    final day = data['weeks'][0]['days'][0]['day'];
-    final exercises = data['weeks'][0]['days'][0]['exercises'] as List<dynamic>;
-    return {
-      'title': 'M$month-W$week-D$day',
-      'exercises': exercises.map((e) => e as Map<String, dynamic>).toList(),
-    };
+    setState(() {
+      month = data['month'];
+      week = data['weeks'][0]['week'];
+      days = data['weeks'][0]['days'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadWorkout();
+  }
+
+  void nextDay() {
+    setState(() {
+      currentDayIndex = (currentDayIndex + 1) % days.length;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: FutureBuilder<Map<String, dynamic>>(
-          future: loadWorkout(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Errore: ${snapshot.error}'));
-            } else {
-              final title = snapshot.data!['title'];
-              final exercises = snapshot.data!['exercises'] as List<Map<String, dynamic>>;
-              return Scaffold(
-                appBar: AppBar(title: Text(title)),
-                body: ListView.builder(
-                  itemCount: exercises.length,
-                  itemBuilder: (context, index) {
-                    final ex = exercises[index];
-                    return ListTile(
-                      title: Text(ex['name']),
-                      subtitle: Text('${ex['sets']}x${ex['reps']} @ ${ex['weight']}kg - ${ex['variation']}'),
-                    );
-                  },
-                ),
-              );
-            }
-          },
-        ),
+    if (days.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Caricamento...')),
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    final currentDay = days[currentDayIndex];
+    final title = 'M$month-W$week-D${currentDay['day']}';
+    final exercises = currentDay['exercises'] as List<dynamic>;
+
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: ListView.builder(
+        itemCount: exercises.length,
+        itemBuilder: (context, index) {
+          final ex = exercises[index];
+          return ListTile(
+            title: Text(ex['name']),
+            subtitle: Text('${ex['sets']}x${ex['reps']} @ ${ex['weight']}kg - ${ex['variation']}'),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: nextDay,
+        child: Icon(Icons.navigate_next),
       ),
     );
   }
