371f1e563171f850f395351c703f9f8a81d453c8
Mang00o <soci.alex24@gmail.com>
2025-06-09 23:05:55 +0200

    creato collection tu gym pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'pages/workout_page.dart';
+import 'pages/tracker_page.dart';
+import 'pages/statistic_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -45,7 +48,62 @@ class HomePage extends StatelessWidget {
             IconButton(
               icon: const Icon(Icons.fitness_center),
               onPressed: () {
-                // TODO: apri funzionalità allenamento
+                showModalBottomSheet(
+                  context: context,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(16),
+                    ),
+                  ),
+                  builder:
+                      (context) => Directionality(
+                        textDirection: TextDirection.rtl,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ListTile(
+                              leading: const Icon(Icons.list),
+                              title: const Text('Workout'),
+                              onTap: () {
+                                Navigator.pop(context);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => const WorkoutPage(),
+                                  ),
+                                );
+                              },
+                            ),
+                            ListTile(
+                              leading: const Icon(Icons.edit),
+                              title: const Text('Tracker'),
+                              onTap: () {
+                                Navigator.pop(context);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => const TrackerPage(),
+                                  ),
+                                );
+                              },
+                            ),
+                            ListTile(
+                              leading: const Icon(Icons.bar_chart),
+                              title: const Text('Statistic'),
+                              onTap: () {
+                                Navigator.pop(context);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => const StatisticPage(),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                );
               },
             ),
           ],
